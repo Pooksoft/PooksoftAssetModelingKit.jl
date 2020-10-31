a5b179965da5f59ad13b0c114cde0f8beb28e1ab
varnerlab <jdv27@cornell.edu>
2020-10-31 10:41:48 -0400

    uploaded build
    yes, I'm doing this

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"-","title":"-","text":"sample_underlying_gbm_model","category":"page"},{"location":"#PooksoftAssetModelingKit.sample_underlying_gbm_model","page":"-","title":"PooksoftAssetModelingKit.sample_underlying_gbm_model","text":"sample_underlying_gbm_model\n\nStuff will go here. Awesome stuff, the most beautiful stuff.\n\n\n\n\n\n","category":"function"}]
+}
