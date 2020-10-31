7ac821e6d813758ff1b635b865cc96fe633b9c77
varnerlab <jdv27@cornell.edu>
2020-10-31 10:24:59 -0400

    Update make.jl
    Trying to gen docs

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ makedocs(sitename="TestDocumentation")
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-deploydocs(
-    repo = "github.com/Pooksoft/PooksoftAssetModelingKit.jl",
-    devurl = "stable",
-)
+# deploydocs(
+#     repo = "github.com/Pooksoft/PooksoftAssetModelingKit.jl",
+#     devurl = "stable",
+# )
